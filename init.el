0c43340066f6a5c9d9be20afbad7f54e98a28980
ozaki <fs65ugumori@yahoo.co.jp>
2015-01-04 22:53:26 +0900

    init

---
+++ init.el
@@ -0,0 +1,28 @@
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'scala-mode2)
+    (package-refresh-contents) (package-install 'scala-mode2))
+
+(require 'coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
+
+(add-hook 'coffee-mode-hook
+	    '(lambda() (coffee-custom)))
+
+(require 'highlight-indentation)
+(setq highlight-indentation-offset 2)
+(set-face-background 'highlight-indentation-current-column-face "#ff0000")
+;;(add-hook 'highlight-indentation-mode-hook 'highlight-indentation-current-column-mode)
+;;(add-hook 'coffee-mode-hook 'highlight-indentation-mode)
+;;(add-hook 'sass-mode-hook 'highlight-indentation-mode)
+(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
+(add-hook 'sass-mode-hook   'highlight-indentation-current-column-mode)
