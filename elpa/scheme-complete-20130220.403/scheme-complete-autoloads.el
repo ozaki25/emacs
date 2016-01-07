9042b807176b3c931ae8a3462aa6f997e65e9e1c
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-01-07 09:13:28 +0900

    a

---
+++ elpa/scheme-complete-20130220.403/scheme-complete-autoloads.el
@@ -0,0 +1,16 @@
+;;; scheme-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("scheme-complete.el") (22157 43993 161721
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; scheme-complete-autoloads.el ends here
