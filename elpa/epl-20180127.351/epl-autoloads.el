b23f61a247386d103283496057d4081ec5948787
ozaki <fs65ugumori@yahoo.co.jp>
2018-03-09 15:47:51 +0900

    ?

---
+++ elpa/epl-20180127.351/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (23156 20837 878024 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
